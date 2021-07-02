e0f69eb3b4ff14fe25ab8d4386747b5de886e819
CaiJingFu <cjf0898@gmail.com>
2021-07-02 23:51:23 +0800

    删除无用代码

--- AIDLServiceDemo/app/src/main/java/com/example/aidlservicedemo/AddService.java
+++ AIDLServiceDemo/app/src/main/java/com/example/aidlservicedemo/AddService.java
@@ -1,7 +1,5 @@
 package com.example.aidlservicedemo;
 
-import java.util.List;
-
 import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -18,73 +16,107 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * AIDLServiceDemo
- * 
+ *
  * @author Jin
  * @since 2021/7/1
  */
 public class AddService extends Service {
 
-    public IAdd.Stub mBinder = new IAdd.Stub() {
+  public IAdd.Stub mBinder =
+      new IAdd.Stub() {
         @Override
         public int addNumbers(int num1, int num2) throws RemoteException {
-            Log.d("TAG", "addNumbers() called with: num1 = [" + num1 + "], num2 = [" + num2 + "]");
-            return num1 + num2;
+          Log.d("TAG", "addNumbers() called with: num1 = [" + num1 + "], num2 = [" + num2 + "]");
+          return num1 + num2;
         }
 
         @Override
         public List<String> getStringList() throws RemoteException {
-            return MainActivity.getList();
+          return getList();
         }
 
         @Override
         public List<Person> getPersonList() throws RemoteException {
-            return MainActivity.getPersons();
+          return getPersons();
         }
 
         @Override
         public void placeCall(String number) throws RemoteException {
-            Intent intent = new Intent(Intent.ACTION_CALL);
-            intent.setData(Uri.parse("tel:" + number));
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_FROM_BACKGROUND);
-            if (ActivityCompat.checkSelfPermission(getApplicationContext(),
-                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+          Intent intent = new Intent(Intent.ACTION_CALL);
+          intent.setData(Uri.parse("tel:" + number));
+          intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+          intent.addFlags(Intent.FLAG_FROM_BACKGROUND);
+          if (ActivityCompat.checkSelfPermission(
+                  getApplicationContext(), Manifest.permission.CALL_PHONE)
+              != PackageManager.PERMISSION_GRANTED) {
 
-                Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
-                // use System.currentTimeMillis() to have a unique ID for the pending intent
-                PendingIntent pIntent =
-                    PendingIntent.getActivity(getApplicationContext(), (int)System.currentTimeMillis(), intent1, 0);
+            Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+            // use System.currentTimeMillis() to have a unique ID for the pending intent
+            PendingIntent pIntent =
+                PendingIntent.getActivity(
+                    getApplicationContext(), (int) System.currentTimeMillis(), intent1, 0);
 
-                // build notification
-                // the addAction re-use the same intent to keep the example short
-                Notification n = new Notification.Builder(getApplicationContext()).setContentTitle("AIDL Server App")
-                    .setContentText("Please grant call permission from settings").setSmallIcon(R.drawable.ic_launcher)
-                    .setContentIntent(pIntent).setAutoCancel(true).build();
+            // build notification
+            // the addAction re-use the same intent to keep the example short
+            Notification n =
+                new Notification.Builder(getApplicationContext())
+                    .setContentTitle("AIDL Server App")
+                    .setContentText("Please grant call permission from settings")
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    .setContentIntent(pIntent)
+                    .setAutoCancel(true)
+                    .build();
 
-                NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+            NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-                notificationManager.notify(0, n);
-                return;
-            }
-            startActivity(intent);
+            notificationManager.notify(0, n);
+            return;
+          }
+          startActivity(intent);
         }
 
         @Override
         public void involved(IPlayListener iPlayListener) throws RemoteException {
-            if (iPlayListener != null) {
-                Bundle bundle = new Bundle();
-                bundle.putString("user", "周杰伦");
-                bundle.putInt("age", 55);
-                iPlayListener.onSuccess("你好", bundle);
-            }
+          if (iPlayListener != null) {
+            Bundle bundle = new Bundle();
+            bundle.putString("user", "周杰伦");
+            bundle.putInt("age", 55);
+            iPlayListener.onSuccess("你好", bundle);
+          }
         }
-    };
+      };
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return mBinder;
-    }
+  public static List<String> getList() {
+    List<String> country = new ArrayList<>();
+    country.add("India");
+    country.add("Bhutan");
+    country.add("Nepal");
+    country.add("USA");
+    country.add("Canada");
+    country.add("China");
+    return country;
+  }
+
+  @Nullable
+  @Override
+  public IBinder onBind(Intent intent) {
+    return mBinder;
+  }
+
+  public List<Person> getPersons() {
+    List<Person> person = new ArrayList<>();
+    person.add(new Person("A", 10));
+    person.add(new Person("B", 20));
+    person.add(new Person("C", 30));
+    person.add(new Person("D", 40));
+    person.add(new Person("E", 50));
+    person.add(new Person("F", 60));
+    return person;
+  }
 }
--- AIDLServiceDemo/app/src/main/java/com/example/aidlservicedemo/MainActivity.java
+++ AIDLServiceDemo/app/src/main/java/com/example/aidlservicedemo/MainActivity.java
@@ -1,67 +1,23 @@
 package com.example.aidlservicedemo;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
-
 import android.Manifest;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 /**
  * 主界面
- * 
+ *
  * @author Jin
  * @since 2021/7/1
  */
 public class MainActivity extends AppCompatActivity {
-    private static List<String> country;
-    private static List<Person> person;
 
-    public static List<String> getList() {
-        country = new ArrayList<>();
-        country.add("India");
-        country.add("Bhutan");
-        country.add("Nepal");
-        country.add("USA");
-        country.add("Canada");
-        country.add("China");
-        return country;
-    }
-
-    public static List<Person> getPersons() {
-        person = new ArrayList<>();
-        person.add(new Person("A", 10));
-        person.add(new Person("B", 20));
-        person.add(new Person("C", 30));
-        person.add(new Person("D", 40));
-        person.add(new Person("E", 50));
-        person.add(new Person("F", 60));
-        return person;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Dexter.withActivity(this).withPermission(Manifest.permission.CALL_PHONE).withListener(new PermissionListener() {
-            @Override
-            public void onPermissionGranted(PermissionGrantedResponse response) {}
-
-            @Override
-            public void onPermissionDenied(PermissionDeniedResponse response) {}
-
-            @Override
-            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
-                token.continuePermissionRequest();
-            }
-        }).check();
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CALL_PHONE}, 1000);
+  }
 }
